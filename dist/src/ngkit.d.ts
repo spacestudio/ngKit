56f2bdfdad2d180437a2bffbd449c5ef65d29b3b
Thiery Laverdure <thiery@spacestud.io>
2016-06-17 09:08:21 -0400

    Removed the dist folder.

--- dist/src/ngkit.d.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from './config';
-export declare class ngKit {
-    config: Config;
-    constructor(config: Config);
-    init(options: Object): ngKit;
-}
--- dist/src/services/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './authentication';
-export * from './authorization';
-export * from './http-client';
-export * from './rest-client';
-export * from './token';
