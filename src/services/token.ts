971e58893b53feec483860a748c10c08831fd468
Thiery Laverdure <thiery@spacestud.io>
2016-06-22 12:29:49 -0400

    Updated the token service

--- src/services/token.ts
+++ src/services/token.ts
@@ -43,7 +43,7 @@ export class ngKitToken {
             if (token) {
                 resolve(token);
             } else {
-                resolve(false);
+                reject(false);
             }
         })
     }
