c53b30737c6b5e47c2235b3fbf7efedf3fbe8c17
Thiery Laverdure <thiery@spacestud.io>
2016-11-03 15:46:56 -0400

    Updated the social authentication service.

--- src/services/social-authentication.ts
+++ src/services/social-authentication.ts
@@ -1,4 +1,4 @@
-import hello from 'hellojs';
+import * as hello from 'hellojs';
 import { Authentication } from  './authentication';
 import { Authorization } from './authorization';
 import { Injectable } from '@angular/core';
