b2175270763b64c3f4a71fb5ceae80eef5898950
Thiery Laverdure <thiery@spacestud.io>
2017-06-27 08:59:52 -0400

    Updated version.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Config } from './config';
 export {
-Authentication, Authorization, Cache, Event, Http, Storage, Token
+Authentication, Authorization, Cache, Event, Http, SocialAuthentication,
+Storage, Token
 } from './services/index';
 export { NGKIT_PROVIDERS } from './providers';
 export { Model } from './models';
