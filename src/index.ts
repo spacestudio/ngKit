c48f856d92775674d9cb1f891cd25157e16c8df2
Thiery Laverdure <thiery@spacestud.io>
2019-02-16 12:38:17 -0500

    Export update.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export { Config } from './config';
 export {
-    Authentication, Authorization, Cache, Event, Http, SocialAuthentication,
-    CookieStorage, LocalStorage, Token
+  Authentication, Authorization, Cache, Event, Http, SocialAuthentication,
+  CookieStorage, LocalStorage, Token
 } from './services/index';
 export { AuthGuard, AuthResolveGuard } from './guards/index';
 export { NGKIT_PROVIDERS } from './providers';
-export { Model } from './models';
+export { Model, ModelFactory } from './models';
 export { ngKitModule } from './ngkit.module';
