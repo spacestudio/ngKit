3700bc6cd5699d4ddb93b9ca085b6242119f806d
Thiery Laverdure <thiery@spacestud.io>
2019-02-16 12:29:27 -0500

    Export model factory.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,5 @@
 export { CacheItemModel } from './cache-item';
 export { Model } from './model';
+export { ModelFactory } from './model-factory';
 export { PolicyModel } from './policy';
 export { UserModel } from './user';
