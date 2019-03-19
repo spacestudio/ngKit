1c9680393812af1974630dc57525e9ca848109f4
Thiery Laverdure <thiery@spacestud.io>
2019-03-19 08:43:03 -0400

    Create model with an instance of itself.

--- src/models/model.ts
+++ src/models/model.ts
@@ -3,6 +3,10 @@ export class Model {
    * Create a new instance of the model.
    */
   constructor(attributes?: any) {
+    if (attributes instanceof this.constructor) {
+      return Object.assign(this, attributes);
+    }
+
     if (typeof attributes === 'string') {
       attributes = JSON.parse(attributes);
     }
