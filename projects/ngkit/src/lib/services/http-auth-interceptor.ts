c44561927e7a508061a0166ce9d16b7a2580423f
Thiery Laverdure <thiery@spacestud.io>
2020-11-17 08:12:26 -0500

    Add auth expired event

--- projects/ngkit/src/lib/services/http-auth-interceptor.ts
+++ projects/ngkit/src/lib/services/http-auth-interceptor.ts
@@ -1,32 +1,45 @@
-import { Injectable } from '@angular/core';
-import { Http } from './http';
 import { Event } from './event';
-import {
-  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
-} from '@angular/common/http';
+import { Http } from './http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpErrorResponse,
+} from "@angular/common/http";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   /**
    * Create a new instance of the interceptor.
    */
-  constructor(
-    public http: Http,
-    public event: Event,
-  ) { }
+  constructor(public http: Http, public event: Event) {}
 
   /**
    * Intercept the http request.
    */
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(tap(() => { }, (error: any) => {
-      if (error instanceof HttpErrorResponse) {
-        if (error.status === 401) {
-          this.event.broadcast('auth:required', error);
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      tap(
+        () => {},
+        (error: any) => {
+          if (error instanceof HttpErrorResponse) {
+            if (error.status === 401) {
+              this.event.broadcast("auth:required", error);
+            }
+
+            if (error.status === 419) {
+              this.event.broadcast("auth:expired", error);
+            }
+          }
         }
-      }
-    }));
+      )
+    );
   }
 }
